6a9ddbd01046ee4246634f30c5a1004fe6c06099
Wahid Ali <1213089@dtu.edu.eg>
2025-06-11 10:38:48 +0300

    Add Splash screen

--- app/src/main/java/org/mahd_e_learning_platform/presentation/MainActivity.kt
+++ app/src/main/java/org/mahd_e_learning_platform/presentation/MainActivity.kt
@@ -5,15 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import org.mahd_e_learning_platform.presentation.screens.welcome.WelcomeScreen
 import org.mahd_e_learning_platform.ui.theme.MahdELearningPlatformTheme
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+//        splashScreen.setKeepOnScreenCondition {
+//            false
+//        }
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
