93e7106c6406e45ea64539a02a0cf7ce14168993
ESLAM <1213018@dtu.edu.eg>
2025-06-11 22:50:41 +0300

    Add forgot password Screen

--- app/src/main/java/org/mahd_e_learning_platform/presentation/MainActivity.kt
+++ app/src/main/java/org/mahd_e_learning_platform/presentation/MainActivity.kt
@@ -5,12 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.ui.Modifier
 import dagger.hilt.android.AndroidEntryPoint
-import org.mahd_e_learning_platform.presentation.screens.welcome.WelcomeScreen
+import org.mahd_e_learning_platform.presentation.screens.welcome.forgot_password.ForgotPasswordScreen
 import org.mahd_e_learning_platform.ui.theme.MahdELearningPlatformTheme
+
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +18,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MahdELearningPlatformTheme {
-                WelcomeScreen(modifier = Modifier.fillMaxSize().systemBarsPadding())
+                ForgotPasswordScreen(
+                    modifier = Modifier.fillMaxSize().systemBarsPadding(),
+                    onBackToLogin = { /* Navigation logic will go here */ },
+                    onBackClicked = { /* Navigation logic will go here */ }
+                )
             }
         }
     }
