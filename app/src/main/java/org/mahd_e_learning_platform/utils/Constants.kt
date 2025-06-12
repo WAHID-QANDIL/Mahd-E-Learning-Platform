9d161d72446d657a5a8309b01b3d6f7315af2ed8
Wahid Ali <1213089@dtu.edu.eg>
2025-06-12 07:31:09 +0300

    Finish up home categories section

--- app/src/main/java/org/mahd_e_learning_platform/utils/Constants.kt
+++ app/src/main/java/org/mahd_e_learning_platform/utils/Constants.kt
@@ -1,5 +1,7 @@
 package org.mahd_e_learning_platform.utils
 
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 val onBoardingImagehight = 250.dp
+val homeCardDimintions = Pair<Dp, Dp>(210.dp,150.dp)
