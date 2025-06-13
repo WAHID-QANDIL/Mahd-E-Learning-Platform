c177759ea9edd1df7a269cfe40670d9f9aacfd9a
Wahid Ali <1213089@dtu.edu.eg>
2025-06-14 01:08:34 +0300

    Finish HomeScreen Ui and added viewmodel, waiting for fetching from data Source

--- app/src/main/java/org/mahd_e_learning_platform/presentation/MainActivity.kt
+++ app/src/main/java/org/mahd_e_learning_platform/presentation/MainActivity.kt
@@ -5,12 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import org.mahd_e_learning_platform.R
+import org.mahd_e_learning_platform.presentation.screens.home.HomeScreen
 import org.mahd_e_learning_platform.presentation.screens.welcome.forgot_password.ForgotPasswordScreen
 import org.mahd_e_learning_platform.ui.theme.MahdELearningPlatformTheme
 
@@ -32,11 +34,18 @@ class MainActivity : ComponentActivity() {
         )
         setContent {
             MahdELearningPlatformTheme {
-              
-                ForgotPasswordScreen(
-                    modifier = Modifier.fillMaxSize().systemBarsPadding(),
-//                    onBackToLogin = { /* Navigation logic will go here */ },
-//                    onBackClicked = { /* Navigation logic will go here */ }
+
+//                ForgotPasswordScreen(
+//                    modifier = Modifier.fillMaxSize().systemBarsPadding(),
+////                    onBackToLogin = { /* Navigation logic will go here */ },
+////                    onBackClicked = { /* Navigation logic will go here */ }
+//                )
+
+                HomeScreen(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .systemBarsPadding()
+                        .background(MahdELearningPlatformTheme.colors.background)
                 )
             }
         }
