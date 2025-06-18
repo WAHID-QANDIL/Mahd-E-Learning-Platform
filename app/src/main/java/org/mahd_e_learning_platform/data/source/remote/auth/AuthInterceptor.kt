fcc551fda962742e8222e3176fb4614205e0a65b
Wahid Ali <1213089@dtu.edu.eg>
2025-06-18 23:03:54 +0300

    Remove course count display from CategoriesSection and related components

--- app/src/main/java/org/mahd_e_learning_platform/data/source/remote/auth/AuthInterceptor.kt
+++ app/src/main/java/org/mahd_e_learning_platform/data/source/remote/auth/AuthInterceptor.kt
@@ -1,8 +1,5 @@
 package org.mahd_e_learning_platform.data.source.remote.auth
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import okhttp3.Interceptor
 import okhttp3.Response
 
@@ -10,13 +7,7 @@ class AuthInterceptor(private val secureTokenStore: SecureTokenStore) : Intercep
     override fun intercept(chain: Interceptor.Chain): Response {
 
         val originalRequest = chain.request()
-        val scope = CoroutineScope(context = Dispatchers.IO)
-        var token: String? = null
-        scope.launch {
-             secureTokenStore.accessTokenFlow.collect{
-                token = it
-            }
-        }
+        var token: String? = secureTokenStore.getTokenSynchronously()
         val requestWithToken = token?.let {
             originalRequest.newBuilder()
                 .addHeader("Authorization", "Bearer $it")
--- app/src/main/java/org/mahd_e_learning_platform/presentation/navigation/AppNavigator.kt
+++ app/src/main/java/org/mahd_e_learning_platform/presentation/navigation/AppNavigator.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import org.mahd_e_learning_platform.presentation.common.error.ErrorScreen
 import org.mahd_e_learning_platform.presentation.screens.auth.WelcomeScreen
 import org.mahd_e_learning_platform.presentation.screens.auth.forgot_password.ForgotPasswordScreen
 import org.mahd_e_learning_platform.presentation.screens.home.HomeScreen
@@ -39,6 +40,11 @@ fun AppNavigator(
         composable(route = Screen.ForgetPassword.destination.rout) {
             ForgotPasswordScreen(
                 modifier = modifier,
+                navHostController = navHostController
+            )
+        }
+        composable(route = Screen.Error.destination.rout) {
+            ErrorScreen(
                 navHostController = navHostController
             )
         }
