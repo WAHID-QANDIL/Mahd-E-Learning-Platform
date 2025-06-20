3e93874c1aa3719f22dabb6dd64e3e52ef589116
Wahid Ali <1213089@dtu.edu.eg>
2025-06-20 07:43:16 +0300

    Add user profile retrieval and update methods to Repository interface

--- app/src/main/java/org/mahd_e_learning_platform/domain/repository/Repository.kt
+++ app/src/main/java/org/mahd_e_learning_platform/domain/repository/Repository.kt
@@ -1,8 +1,11 @@
 package org.mahd_e_learning_platform.domain.repository
 
-import org.mahd_e_learning_platform.data.source.remote.model.RegisterRequest
+import org.mahd_e_learning_platform.data.api.model.RegisterRequest
+import org.mahd_e_learning_platform.data.api.model.UserProfile
 
 interface Repository {
     suspend fun login(email: String, password: String)
     suspend fun register(registerRequest: RegisterRequest)
+    suspend fun getUserProfile(): UserProfile
+    suspend fun updateUserProfile(newUserInfo: Map<String, String>)
 }
