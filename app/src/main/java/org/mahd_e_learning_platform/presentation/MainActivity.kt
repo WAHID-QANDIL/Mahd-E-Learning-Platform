29b6067dd8badc212ca0e078b0126dd39c1a6228
Wahid Ali <1213089@dtu.edu.eg>
2025-06-22 04:16:09 +0300

    Refactor MainActivity to conditionally display bottom navigation bar based on current route

--- app/src/main/java/org/mahd_e_learning_platform/presentation/MainActivity.kt
+++ app/src/main/java/org/mahd_e_learning_platform/presentation/MainActivity.kt
@@ -6,13 +6,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
@@ -22,6 +26,7 @@ import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import org.mahd_e_learning_platform.R
 import org.mahd_e_learning_platform.data.source.local.datastore.SecureTokenStore
+import org.mahd_e_learning_platform.presentation.common.AppBottomHomeNavBar
 import org.mahd_e_learning_platform.presentation.navigation.AppNavigator
 import org.mahd_e_learning_platform.presentation.navigation.Screen
 import org.mahd_e_learning_platform.ui.theme.MahdELearningPlatformTheme
@@ -52,10 +57,17 @@ class MainActivity : ComponentActivity() {
                 var isLoggedIn by remember { mutableStateOf(false) }
                 var isFirstLaunch by remember { mutableStateOf(true) }
                 splashScreen.setKeepOnScreenCondition { isLoading }
+                val currentRoute = navHostController.currentDestination?.route
+                val showBottomBar = remember(currentRoute) {
+                    currentRoute == Screen.Home.destination.rout ||
+                            currentRoute == Screen.Search.destination.rout ||
+                            currentRoute == Screen.MyCourses.destination.rout ||
+                            currentRoute == Screen.Profile.destination.rout
+                }
 
 
                 LaunchedEffect(Unit) {
-                    isLoggedIn = secureTokenStore.accessLoginState.first().toBoolean()
+                    isLoggedIn = secureTokenStore.accessLoginState.first()
                     isFirstLaunch = secureTokenStore.accessFirstLaunchState.first()
                     Log.d("isLoggedIn", "onCreate: $isLoggedIn")
                     Log.d("isLoggedIn", "onCreate: $isFirstLaunch")
@@ -74,20 +86,39 @@ class MainActivity : ComponentActivity() {
                     }
                     isLoading = false
                 }
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.SpaceBetween,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
 
-                AppNavigator(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .systemBarsPadding(), navHostController =
-                        navHostController
-                )
+                    AppNavigator(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(9f)
+                            .systemBarsPadding(), navHostController =
+                            navHostController
+                    )
+                    if (showBottomBar) {
+                        AppBottomHomeNavBar(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(1f),
+                            navHostController = navHostController
+                        )
+                    }
+
+
+                }
+
+
             }
         }
     }
 
     override fun onStart() {
         super.onStart()
-        lifecycleScope.launch{
+        lifecycleScope.launch {
             secureTokenStore.saveFirstLaunch(false)
         }
 
