4b229adfd9b30ccaf6a0857fe73ddd878a15e68d
Wahid Ali <1213089@dtu.edu.eg>
2025-06-22 04:13:58 +0300

    Add navigation for Profile, CourseDetails, CoursePlayer, and Search screens

--- app/src/main/java/org/mahd_e_learning_platform/presentation/navigation/AppNavigator.kt
+++ app/src/main/java/org/mahd_e_learning_platform/presentation/navigation/AppNavigator.kt
@@ -8,21 +8,26 @@ import androidx.navigation.compose.composable
 import org.mahd_e_learning_platform.presentation.common.error.ErrorScreen
 import org.mahd_e_learning_platform.presentation.screens.auth.WelcomeScreen
 import org.mahd_e_learning_platform.presentation.screens.auth.forgot_password.ForgotPasswordScreen
+import org.mahd_e_learning_platform.presentation.screens.auth.verification.VerificationScreen
+import org.mahd_e_learning_platform.presentation.screens.course_details.CourseDetailsScreen
+import org.mahd_e_learning_platform.presentation.screens.course_player.CoursePlayerScreen
 import org.mahd_e_learning_platform.presentation.screens.home.HomeScreen
 import org.mahd_e_learning_platform.presentation.screens.onboarding.OnBoarding
+import org.mahd_e_learning_platform.presentation.screens.profile.profile_menu.ProfileScreen
+import org.mahd_e_learning_platform.presentation.screens.search.SearchScreen
 
 
 @Composable
 fun AppNavigator(
     modifier: Modifier = Modifier,
-    navHostController: NavHostController
-    ) {
+    navHostController: NavHostController,
+) {
 
 
     NavHost(
         navController = navHostController,
         startDestination = Screen.Welcome.destination.rout
-    ){
+    ) {
 
         composable(route = Screen.Welcome.destination.rout) {
             WelcomeScreen(
@@ -46,12 +51,58 @@ fun AppNavigator(
         }
         composable(route = Screen.Error.destination.rout) {
             ErrorScreen(
+                modifier = modifier,
                 navHostController = navHostController
             )
         }
         composable(route = Screen.OnBoarding.destination.rout) {
             OnBoarding(
+                modifier = modifier,
                 navHostController = navHostController
+            )
+        }
+        composable(route = Screen.Profile.destination.rout) {
+            ProfileScreen(
+                modifier = modifier,
+                navHostController = navHostController
+            )
+        }
+
+        composable(route = Screen.Profile.destination.rout) {
+            ProfileScreen(
+                modifier = modifier,
+                navHostController = navHostController
+            )
+        }
+
+        composable(route = Screen.OtpVerification.destination.rout) {
+            VerificationScreen(
+                modifier = modifier,
+                navHostController = navHostController
+            )
+        }
+        composable(route = Screen.OtpVerification.destination.rout) {
+            VerificationScreen(
+                modifier = modifier,
+                navHostController = navHostController
+            )
+        }
+        composable(route = Screen.CourseOverview.destination.rout) {
+            CourseDetailsScreen(
+                modifier = modifier,
+                navHostController = navHostController
+            )
+        }
+
+        composable(route = Screen.CourseContent.destination.rout) {
+            CoursePlayerScreen(
+                modifier = modifier,
+                navHostController = navHostController
+            )
+        }
+        composable(route = Screen.Search.destination.rout) {
+            SearchScreen(
+                modifier = modifier,
             )
         }
 
